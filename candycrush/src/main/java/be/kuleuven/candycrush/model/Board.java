eee4a200bea111740c86654fc0f51abf32b89416
IliasDeBisschop <ilias.debisschop@student.kuleuven.be>
2024-05-09 10:57:57 +0200

    start 11

--- candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
+++ candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
+// start
 public class Board<E> {
     public Map<Position, E> board;
     public Map<E, Position> reverseBoard;
